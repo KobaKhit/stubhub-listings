f85c294a09bab6005b042f6d24812bb27c717fe2
Kobakhit <kobakhit@gmail.com>
2018-03-21 11:35:16 -0400

    added open graph tags

--- dash_responsive.py
+++ dash_responsive.py
@@ -12,13 +12,13 @@ class Dash_responsive(dash.Dash):
         title = getattr(self, 'title', 'Dash')
         return ('''
         <!DOCTYPE html>
-        <html prefix="og: http://ogp.me/ns#">
+        <html>
             <head>
                 <meta prefix="og: http://ogp.me/ns#" property="og:title" content="{}"/>
                 <meta prefix="og: http://ogp.me/ns#" property="og:url" content="https://stubhub-listings.herokuapp.com"/>
                 <meta prefix="og: http://ogp.me/ns#" property="og:description"
                   content="This app enables you to download all stubhub listings for a given event id."/>
-                <meta prefix="og: http://ogp.me/ns#" property="og:image" content="https://cdn.rawgit.com/KobaKhit/stubhubAPI/dc4969bc/readme_plots/heatmap.png"/>
+                <meta prefix="og: http://ogp.me/ns#" property="og:image" content="https://cdn.rawgit.com/KobaKhit/stubhubAPI/4a800a32/readme_plots/heatmap.png"/>
                 <title>{}</title>
                 {}
             </head>
